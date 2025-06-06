e753176d02b7edc5fbd9598784837ab25805e1b4
niyozbek0815 <niyozbekmaxammadjonov@gmail.com>
2025-06-06 20:49:09 +0500

    Restore src folder to initial commit state

---
+++ notification-service/src/app/Jobs/TestJob.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(public $data = [])
+    {
+        //
+    }
+
+    public function handle(): void
+    {
+        Log::info('✅ TestJob handled successfully!', ['data' => $this->data]);
+    }
+}
