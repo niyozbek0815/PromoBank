ed94eb0a023d9ada9180d8b2c20ec49f5c4dc502
niyozbek0815 <niyozbekmaxammadjonov@gmail.com>
2025-11-15 21:34:18 +0500

    commit

--- promo-service/src/database/seeders/CompaniesUsersSeeder.php
+++ promo-service/src/database/seeders/CompaniesUsersSeeder.php
@@ -14,12 +14,12 @@ class CompaniesUsersSeeder extends Seeder
     public function run(): void
     {
         $bindings = [
-            ['company_id' => 1, 'user_id' => 101],
-            ['company_id' => 1, 'user_id' => 102],
-            ['company_id' => 2, 'user_id' => 103],
-            ['company_id' => 2, 'user_id' => 101],
-            ['company_id' => 3, 'user_id' => 104],
-            ['company_id' => 3, 'user_id' => 102],
+            ['company_id' => 1, 'user_id' => 12],
+            ['company_id' => 1, 'user_id' => 13],
+            ['company_id' => 2, 'user_id' => 14],
+            ['company_id' => 2, 'user_id' => 15],
+            ['company_id' => 3, 'user_id' => 16],
+            ['company_id' => 3, 'user_id' => 17],
         ];
 
         DB::table('companies_users')->insert($bindings);
